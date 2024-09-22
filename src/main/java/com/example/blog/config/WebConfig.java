170014b3d05dbc17554092abf44f13fb5c735334
snow <doufu6640@gmail.com>
2024-09-22 15:57:26 +0800

    test

--- src/main/java/com/example/blog/config/WebConfig.java
+++ src/main/java/com/example/blog/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.example.blog.config;
 import com.example.blog.interceptor.UserInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
@@ -15,4 +16,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(userInterceptor).
                 excludePathPatterns("/**");
     }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:*") // 允许来自任意端口的localhost请求
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+
 }
